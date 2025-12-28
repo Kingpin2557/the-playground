200d80ca29c7455ec45c344bfea94a2874c98b4a
Kingpin2557 <lode.snaet@student.howest.be>
2025-12-28 16:54:13 +0100

    Updated default camera position and target in `useCameraStore`; minor whitespace cleanup in `App`.

--- src/store/useCameraStore.ts
+++ src/store/useCameraStore.ts
@@ -17,7 +17,7 @@ export const useCameraStore = create<CameraStore>(() => ({
         fov: 45,
         near: 0.1,
         far: 1000,
-        position: [0, 8, 10],
-        target: [0, 0, 0],
+        position: [4.599999999999976, 2.1, -0.8999999999999848],
+        target: [0.19999999999999998, 0.5, -0.6],
     },
 }));
